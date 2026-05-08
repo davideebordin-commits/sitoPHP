160ef9e5497f233fbaf1afe2dad84f3a629b4534
david <davideebordin@gmail.com>
2026-05-08 18:49:52 +0200

    fix full screen mobile

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 <link rel="stylesheet" href="grafica.css">
 
 <head>
+  <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100%;
+    overflow-x: hidden;
+  }
+</style>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Wallah Kebab</title>
